13fae412ca5121923538cf8c5a755de993b9d154
Bhav Beri <bhavberi@gmail.com>
2024-02-10 14:14:04 +0530

    Added docker-compose.prod

--- run.sh
+++ run.sh
@@ -4,11 +4,18 @@ IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
 # Write environment variables to .env file
 cat << EOF > ./web/.env
-GRAPHQL_ENDPOINT="http://${IP_ADDRESS}/graphql"
-NEXT_PUBLIC_FILESERVER_URL="http://${IP_ADDRESS}"
-NEXT_PUBLIC_STATIC_URL="http://${IP_ADDRESS}/static"
-NEXT_PUBLIC_CLUB_ID="osdg"
+GRAPHQL_ENDPOINT="https://clubs.iiit.ac.in/graphql"
+NEXT_PUBLIC_FILESERVER_URL="https://clubs.iiit.ac.in"
+NEXT_PUBLIC_STATIC_URL="https://clubs.iiit.ac.in/static"
+NEXT_PUBLIC_CLUB_ID="hacking.club"
 EOF
+
+# cat << EOF > ./web/.env
+# GRAPHQL_ENDPOINT="http://${IP_ADDRESS}/graphql"
+# NEXT_PUBLIC_FILESERVER_URL="http://${IP_ADDRESS}"
+# NEXT_PUBLIC_STATIC_URL="http://${IP_ADDRESS}/static"
+# NEXT_PUBLIC_CLUB_ID="osdg"
+# EOF
 
 FILE_PATH="web/next.config.js"
 
